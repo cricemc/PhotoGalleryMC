4f6850b6f195cb7b51f691e4b1b783c058bd7a39
student <student@300-Conf>
2018-10-08 18:34:40 -0500

    Add PhotoDbContext and add the DbContext to the services container to make it available via dependency injection.

--- PhotoGallery/Startup.cs
+++ PhotoGallery/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PhotoGallery.Models;
 
 namespace PhotoGallery
 {
@@ -24,6 +26,7 @@ namespace PhotoGallery
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PhotoDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
