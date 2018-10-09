c87a07fe7cd89b1a326730299ba2553d8a44c223
student <student@300-Conf>
2018-10-08 21:30:32 -0500

    Upload a photo from the user and store it in Azure Storage and create a database record.

--- PhotoGallery/Controllers/HomeController.cs
+++ PhotoGallery/Controllers/HomeController.cs
@@ -3,18 +3,54 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PhotoGallery.Models;
+using PhotoGallery.Services;
 
 namespace PhotoGallery.Controllers
 {
     public class HomeController : Controller
     {
+        private PhotoDbContext _dbContext;
+        private PhotoImageStorageService _photoImageStorageService;
+
+        public HomeController(PhotoDbContext dbContext, PhotoImageStorageService photoImageStorageService)
+        {
+            _dbContext = dbContext;
+            _photoImageStorageService = photoImageStorageService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
         
+        public async Task<IActionResult> Upload(IFormCollection formCollection)
+        {
+            try
+            {
+                var file = formCollection.Files.FirstOrDefault();
+
+                // Store photo to Azure Storage
+                string imageIdentifier = await _photoImageStorageService.SaveImage(file?.OpenReadStream(), DateTime.UtcNow.Millisecond.ToString() + file?.FileName);
+
+                // Update database
+                _dbContext.PhotoImages.Add(new PhotoImage
+                {
+                    Url = imageIdentifier,
+                    Caption = "None"
+                });
+
+                await _dbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error");
+            }
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
